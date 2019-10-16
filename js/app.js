c6e39e94ccc66cbdd2cc2fcc27781e6c2f4de6f8
paul bennett <luapkb@gmail.com>
2019-10-16 10:25:49 -0700

    end paul drive

--- js/app.js
+++ js/app.js
@@ -11,25 +11,18 @@ function Horn(title, image_url, description, keyword, horns) {
   allHorns.push(this);
 }
 Horn.prototype.render = function(){
-  // select all the html in the dog template
   const myTemplate = $('#photo-template').html();
 
-  // make a new section to hold my template
   const $newSection = $('<section></section>');
 
-  // put the html from the dog template into the new section
   $newSection.html(myTemplate);
 
-  // find the h2 and fill the text with the title
   $newSection.find('h2').text(this.title);
 
-  // find the p tag and fill with description
   $newSection.find('p').text(this.description);
 
-  // find the image_url and fill with the image url
   $newSection.find('img').attr('src', this.image_url);
 
-  // stick my template onto the DOM
   $('main').append($newSection);
 };
 
@@ -45,7 +38,12 @@ function fetchdata(){
   
 }
 
-
+function clickHandler(event){
+  event.preventDefault();
+  let keyValue = event.target.value;
+  // ref https://stackoverflow.com/questions/178407/select-all-child-elements-except-the-first
+  $('section:not(:first-child)').remove();
+}
 $(function() {
   fetchdata();
 
