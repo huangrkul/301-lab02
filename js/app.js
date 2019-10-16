59a1df29682267565bedbacc2a776f9c4763afaa
huangrkul <huangrkul@gmail.com>
2019-10-16 10:53:46 -0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -12,19 +12,17 @@ function Horn(title, image_url, description, keyword, horns) {
 }
 Horn.prototype.render = function(){
   const myTemplate = $('#photo-template').html();
-
   const $newSection = $('<section></section>');
-
   $newSection.html(myTemplate);
-
   $newSection.find('h2').text(this.title);
-
   $newSection.find('p').text(this.description);
-
   $newSection.find('img').attr('src', this.image_url);
-
   $('main').append($newSection);
 };
+
+function optionRender() {
+
+}
 
 function fetchdata(){
   $.get('data/page-1.json', data => {
@@ -32,10 +30,10 @@ function fetchdata(){
       new Horn(horn.title, horn.image_url, horn.description, horn.keyword, horn.horns).render();
       if( data.length === allHorns.length){
         $('select').on('change', clickHandler);
+        optionRender();
       }
     });
   });
-  
 }
 
 function clickHandler(event){
@@ -43,6 +41,11 @@ function clickHandler(event){
   let keyValue = event.target.value;
   // ref https://stackoverflow.com/questions/178407/select-all-child-elements-except-the-first
   $('section:not(:first-child)').remove();
+  allHorns.forEach(isolate => {
+    if(isolate.keyword === keyValue) {
+      isolate.render();
+    }
+  });
 }
 $(function() {
   fetchdata();
